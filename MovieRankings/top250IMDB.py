48119c2cfa578cdc9131f552fb65639f8320dcbd
Yiyang <yiyangzhou123@ucla.edu>
2020-07-08 21:05:37 -0700

    get movies' rank, title and link

--- MovieRankings/top250IMDB.py
+++ MovieRankings/top250IMDB.py
@@ -1,3 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
+URL="https://www.imdb.com/chart/top/"
+imdbURL="www.imdb.com"
+req_params={"ref":"nv_mv_250"}
+
+res=requests.get(URL,params=req_params)
+print(res.url)
+print(res.status_code)
+page_html= res.text
+
+soup = BeautifulSoup(page_html, "html.parser")
+table=soup.find("tbody",class_="lister-list")
+tableRows=table.findAll("tr")
+movieList=[]
+for row in tableRows:
+    rank=row.find("span",attrs={"name":"rk"})["data-value"]
+    titleColumn=row.find("td",class_="titleColumn")
+    link=imdbURL+titleColumn.a["href"]
+    title=titleColumn.a.string
+    movieList.append({"rank":rank,"title":title,"link":link})
+
+print(movieList)
+    
+
